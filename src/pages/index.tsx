2ba3a5ad680bfb72d47c4dff4f0e50de9476bb1c
Sheheryar Abid <Sheheryar.Abid@wilsongroupau.com>
2025-04-22 12:08:43 +0800

    Initial commit for cloud deployment

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen text-2xl">
+      <h1>Clinic IT Solutions Coming Soon</h1>
+    </main>
+  );
+}
